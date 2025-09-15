26be1f7500c71306a7e033c767d2616ce77d2963
Seema Anjum <234g1a33f6@srit.ac.in>
2025-09-15 15:53:47 +0530

    commit

---
+++ my-counter-app/src/temp.jsx
@@ -0,0 +1,11 @@
+function welcome(prop){
+    return <h2>welcome (prop,name)!</h2>;
+}
+function temp(){
+    return(
+        <div>
+            <welcome name="seema"/>
+            <welcome name="Anjum"/>
+        </div>
+    )
+}
